bc0c324f2ec09ee554617b2228e3111acfbc0e79
Toku <wwwtoctoc@gmail.com>
2020-07-06 14:16:35 +0000

    implement advanced login

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   post '/signup',  to: 'users#create'
   get '/login', to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
-  delete '/logout',  to:'sessions#destroy'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
